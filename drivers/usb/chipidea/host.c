857745ca06a6a7c3b051f4a14643b866b5b91c6b
Stephen Boyd <stephen.boyd@linaro.org>
2016-09-07 14:35:04 -0700

    usb: chipidea: Notify events when switching host mode
    The chipidea/udc.c file sends a CI_HDRC_CONTROLLER_RESET_EVENT to
    the wrapper drivers when it calls hw_device_reset(), but that
    function is not called from chipidea/host.c. And the udc.c file
    sends the CI_HDRC_CONTROLLER_STOPPED_EVENT but the host.c file
    doesn't do anything.
    
    The intent of the reset event is to allow the wrapper driver to
    do any wrapper specific things after the reset bit has been set
    in the usb command register. Therefore, add this event hook in
    the host role after we toggle that bit.
    
    Similarly, the intent of the stopped event is to allow the
    wrapper driver to do any wrapper specific things after the device
    is stopped. So when we stop the host role, send the stopped
    event.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Stephen Boyd <stephen.boyd@linaro.org>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/chipidea/host.c
+++ drivers/usb/chipidea/host.c
@@ -89,6 +89,8 @@ static int ehci_ci_reset(struct usb_hcd *hcd)
 		return ret;
 
 	ehci->need_io_watchdog = 0;
+	if (ci->platdata->notify_event)
+		ci->platdata->notify_event(ci, CI_HDRC_CONTROLLER_RESET_EVENT);
 
 	ci_platform_configure(ci);
 
@@ -187,6 +189,9 @@ static void host_stop(struct ci_hdrc *ci)
 	struct usb_hcd *hcd = ci->hcd;
 
 	if (hcd) {
+		if (ci->platdata->notify_event)
+			ci->platdata->notify_event(ci,
+				CI_HDRC_CONTROLLER_STOPPED_EVENT);
 		usb_remove_hcd(hcd);
 		usb_put_hcd(hcd);
 		if (ci->platdata->reg_vbus && !ci_otg_is_fsm_mode(ci) &&
